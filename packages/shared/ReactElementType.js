fa74414f9273b85df39f7f50a23797c8068cd045
cpk02088808 <cpk02088808@taobao.com>
2025-06-05 16:43:48 +0800

    feat: 元素对象说明

--- packages/shared/ReactElementType.js
+++ packages/shared/ReactElementType.js
@@ -10,10 +10,15 @@
 import type {ReactDebugInfo} from './ReactTypes';
 
 export type ReactElement = {
+  // 用于安全目的的符号
   $$typeof: any,
+  // 字符串（用于 DOM 元素）或函数/类（用于组件）
   type: any,
+  // 可选的唯一标识符，帮助 React 在协调期间识别元素
   key: any,
+  // 访问实际 DOM 节点或组件实例的可选引用
   ref: any,
+  // 包含此元素的属性和子元素的对象
   props: any,
   // __DEV__ or for string refs
   _owner: any,
